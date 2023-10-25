c86828aff46e71aa964d45ed571b6001736eaa53
ladronbx <ladronbravovlc@gmail.com>
2023-10-25 18:37:21 +0200

    feat: Initialize AppDataSource to connect to the database

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
 import express from "express";
+import { AppDataSource } from "./db";
 
 const app = express();
 const PORT = process.env.PORT || 4000;
 
 app.use(express.json());
-app.listen(PORT, () => {
-    console.log(`Server running ${PORT}`);
-});
+
+AppDataSource.initialize()
+    .then(() => {
+        console.log('Database connected');
+    })
+    .catch(error => {
+        console.log(error);
+    });
