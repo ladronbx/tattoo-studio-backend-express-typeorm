d15e1a0054dc1a8da5ec6ae35f168bb0fef84a59
ladronbx <ladronbravovlc@gmail.com>
2023-10-27 22:16:54 +0200

    refactor: Stop tracking .DS_Store file

--- src/db.ts
+++ src/db.ts
@@ -1,13 +1,5 @@
 import "reflect-metadata";
 import { DataSource } from "typeorm";
-import { CreateUsersTable1698295901193 } from "./migration/1698295901193-create-users-table";
-import { CreateRolesTable1698296931960 } from "./migration/1698296931960-create-roles-table";
-import { CreatePortfoliosTable1698297252842 } from "./migration/1698297252842-create-portfolios-table";
-import { CreateClientsTable1698297600908 } from "./migration/1698297600908-create-clients-table";
-import { CreateArtistsTable1698297698007 } from "./migration/1698297698007-create-artists-table";
-import { CreatePortfolioArtistTable1698298091237 } from "./migration/1698298091237-create-portfolio_artist-table";
-import { CreateAppointmentsTable1698298269709 } from "./migration/1698298269709-create-appointments-table";
-import { CreateRoleUserTable1698337740215 } from "./migration/1698337740215-create-role_user-table";
 import { User } from "./models/User";
 import { Artist } from "./models/Artist";
 import { Client } from "./models/Client";
@@ -25,15 +17,6 @@ const AppDataSource = new DataSource({
     password: "password",
     database: "tattoo_studio_db",
     migrations: [
-        CreateUsersTable1698295901193,
-        CreateRolesTable1698296931960,
-        CreatePortfoliosTable1698297252842,
-        CreateClientsTable1698297600908,
-        CreateArtistsTable1698297698007,
-        CreatePortfolioArtistTable1698298091237,
-        CreateAppointmentsTable1698298269709,
-        CreateRoleUserTable1698337740215
-        
 
     ],
     entities: [User, Artist, Client, Portfolio, Portfolio_artist, Role_user, Role, User, Appointment],
