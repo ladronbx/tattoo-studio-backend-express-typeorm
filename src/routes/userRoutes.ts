af0c5ac1143811299b673b0c70ecda5bffdcccdd
ladronbx <ladronbravovlc@gmail.com>
2023-10-27 22:12:46 +0200

    refactor: Remove all migrations and models due to table design error

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,12 +1,5 @@
 import { Router } from "express";
-import { createUser, deleteUser, getUser, updateUser, register } from "../controllers/userControllers";
 
 const routerUsers = Router ();
-
-routerUsers.get('/user', getUser);
-routerUsers.put('/', updateUser);
-//agrego registro
-routerUsers.post('/register', register);
-routerUsers.delete('/', deleteUser);
 
 export {routerUsers}
