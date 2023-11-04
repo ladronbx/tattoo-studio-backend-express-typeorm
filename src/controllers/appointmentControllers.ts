3fe3f8706d63e2539f9d464f953a479f8b578893
ladronbx <ladronbravovlc@gmail.com>
2023-11-04 10:41:30 +0100

    [feat] : Updated validation logic in updateAppointment function
    - Enhanced validation logic within the update Appointment function to ensure accurate and secure appointment updates.
    - Added checks to ensure that the updated appointment details meet specific criteria, such as valid date, shift, and email format.

--- src/controllers/appointmentControllers.ts
+++ src/controllers/appointmentControllers.ts
@@ -131,7 +131,8 @@ const myCalendarAsArtist = async (req: Request, res: Response) => {
     try {
         const id = req.token.id;
 
-        const appointmentsForShows = await Appointment.find({ where: {artist_id: id},
+        const appointmentsForShows = await Appointment.find({
+            where: { artist_id: id },
             select: ["id", "date", "shift", "client_id", "status"],
         });
 
@@ -150,6 +151,7 @@ const myCalendarAsArtist = async (req: Request, res: Response) => {
     }
 }
 
+//Elimina la cita por id
 const deleteAppointment = async (req: Request, res: Response) => {
     try {
         const appointmentId = req.body.id;
@@ -200,234 +202,38 @@ const deleteAppointment = async (req: Request, res: Response) => {
     }
 }
 
-const getAllMyAppointments = async (req: Request, res: Response) => {
-
-    try {
-        const clientTokenId = req.token.id;
-
-        const userAppointments = await Appointment.findBy({
-            client_id: clientTokenId
-        });
-
-        const formattedAppointments = await Promise.all(userAppointments.map(async (appointment) => {
-            const { status, artist_id, client_id, ...appointmentDetails } = appointment;
-
-            const artistDetails = await User.findOneBy({
-                id: artist_id
-            });
-
-            if (artistDetails) {
-                const full_name = artistDetails.full_name
-                const artistEmail = artistDetails.email;
-                const artistIsActive = artistDetails.is_active;
-                return { full_name, artistEmail, artistIsActive };
-            } else {
-                return null;
-            }
-        }));
-
-        return res.json({
-            success: true,
-            message: "Here is a list of all your appointments",
-            data: formattedAppointments
-        });
-
-    } catch (error) {
-        return res.json({
-            success: false,
-            message: "There was an issue retrieving your appointments, please try again",
-            error
-        });
-    }
-}
-
-const updateAppointment = async (req: Request, res: Response) => {
-    try {
-        const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-        const dateRegex = /^\d{4}-\d{2}-\d{2}$/;
-        const client_id = req.token.id
-        const body = req.body
-        const appointmentId = body.id
-        const date = body.date
-        const shift = body.shift
-        const email = body.email
-
-        if (!email) {
-            return res.json({
-                success: true,
-                message: "you must insert an email",
-            })
-        }
-
-        if (typeof (email) !== "string") {
-            return res.json({
-                success: true,
-                message: 'Email incorrect, you can only enter strings. Please try again.'
-            });
-        }
-
-        if (email.length > 100) {
-            return res.json({
-                success: true,
-                message: 'Email too long, please insert a shorter name, maximum 100 characters.'
-            });
-        }
-
-        if (!emailRegex.test(email)) {
-            return res.json({
-                success: true,
-                message: 'Email format incorrect, try again'
-            });
-        }
-
-        const findArtistByEmail = await User.findOneBy({
-            email
-        })
-
-        if (findArtistByEmail?.is_active !== true) {
-            return res.json({
-                success: true,
-                message: "Artist doesn't exist"
-            })
-        }
-
-        const WorkerID = findArtistByEmail?.id
-
-        if (!appointmentId) {
-            return res.json({
-                success: true,
-                message: "You must enter an ID.",
-            })
-        }
-
-        if (typeof (appointmentId) !== "number") {
-            return res.json({
-                success: true,
-                message: "ID incorrect, you can only use numbers, please try again."
-            });
-        }
-
-        if (!date) {
-            return res.json({
-                success: true,
-                message: "Date not provided",
-            })
-        }
-
-        if (typeof (date) !== "string") {
-            return res.json({
-                success: true,
-                message: "Incorrect date format. Date must be a string"
-            });
-        }
-
-        if (!dateRegex.test(date)) {
-            return res.json({
-                success: true,
-                message: "Incorrect date, the format should be YYYY-MM-DD."
-            });
-        }
-
-        if (!shift) {
-            return res.json({
-                success: true,
-                message: "Time not provided",
-            })
-        }
-
-        if (typeof (shift) !== "string") {
-            return res.json({
-                success: true,
-                message: "Time must be a string."
-            });
-        }
-
-        const appointmentsClient = await Appointment.findBy({
-            client_id,
-        })
-
-        const appointmentsId = await appointmentsClient.map((object) =>
-            object.id
-        )
-
-        if (!appointmentsId.includes(appointmentId)) {
-            return res.json(
-                {
-                    success: true,
-                    message: "Appointment not updated successfully, incorrect ID."
-                }
-            )
-        }
-
-        await Appointment.update(
-            {
-                id: appointmentId
-            },
-            {
-                date: date,
-                shift: shift,
-                artist_id: WorkerID
-            }
-        )
-
-        const dataAppointmentUpdated = await Appointment.findOneBy({
-            id: appointmentId
-        })
-
-        return res.json({
-            success: true,
-            message: "The appointment was successfully created.",
-            data: {
-                date,
-                shift,
-                email,
-                id: appointmentId,
-                created_at: dataAppointmentUpdated?.created_at,
-                updated_at: dataAppointmentUpdated?.updated_at
-            }
-        })
-
-    } catch (error) {
-        return res.json(
-            {
-                success: false,
-                message: "Updating the appointment is currently not possible. Please try again.",
-                error
-            }
-        )
-    }
-}
-
 // obtener todas las citas unicamente con el rol super admin 
-const getallAppointmentsAllUsers = async (req: Request, res: Response) => {
+const getAllAppointmentsCalendar = async (req: Request, res: Response) => {
 
     try {
-        const id = req.token.id
 
-        const appointmentsUser = await Appointment.find()
+        if (typeof (req.query.skip) !== "string") {
+            return res.json({
+                success: true,
+                message: "skip it's not string."
+            })
+        }
 
-        const appointmentsUserForShows = await Promise.all(appointmentsUser.map(async (obj) => {
-            const { status, artist_id, client_id, ...rest } = obj;
+        if (typeof (req.query.page) !== "string") {
+            return res.json({
+                success: true,
+                message: "page it's not string."
+            })
+        }
 
-            const user = await User.findOneBy({
-                id: client_id
-            });
+        const pageSize = parseInt(req.query.skip as string) || 5
+        const page: any = parseInt(req.query.page as string) || 1
+        const skip = (page - 1) * pageSize
 
-            if (user) {
-                const email = user.email;
-                const is_active = user.is_active;
-                const full_name = user.full_name;
-                return { email, is_active, full_name, ...rest, };
-            }
-            else {
-                return null
-            }
-        }));
+        const appointmentsUser = await Appointment.find({
+            skip: skip,
+            take: pageSize
+        })
 
         return res.json({
             success: true,
             message: "Here are all your appointments",
-            data: appointmentsUserForShows
+            data: appointmentsUser
         });
 
     } catch (error) {
@@ -439,4 +245,255 @@ const getallAppointmentsAllUsers = async (req: Request, res: Response) => {
     }
 }
 
-export { createAppointment, myCalendarAsArtist, deleteAppointment, getAllMyAppointments, updateAppointment, getallAppointmentsAllUsers }
+// obtener todas las citas unicamente con el rol super admin en detalle (incluyendo detalles)
+const getAllAppointmentsCalendarDetails = async (req: Request, res: Response) => {
+
+    try {
+
+        if (typeof (req.query.skip) !== "string") {
+            return res.json({
+                success: true,
+                message: "skip it's not string."
+            })
+        }
+
+        if (typeof (req.query.page) !== "string") {
+            return res.json({
+                success: true,
+                message: "page it's not string."
+            })
+        }
+
+        const pageSize = parseInt(req.query.skip as string) || 5
+        const page: any = parseInt(req.query.page as string) || 1
+        const skip = (page - 1) * pageSize
+
+        const appointmentsUser = await Appointment.find({
+            relations: ["appointmentPortfolios"],
+            skip: skip,
+            take: pageSize
+        })
+
+        return res.json({
+            success: true,
+            message: "Here are all your appointments",
+            data: appointmentsUser
+        });
+
+    } catch (error) {
+        return res.json({
+            success: false,
+            message: "appointments can't be getted, try again",
+            error
+        })
+    }
+}
+
+// Trae todos los datos necesarios del cliente que lo solicita
+const getAllMyAppointments = async (req: Request, res: Response) => {
+
+    try {
+        const idToken = req.token.id
+
+        if (typeof (req.query.skip) !== "string") {
+            return res.json({
+                success: true,
+                message: "skip it's must a number."
+            })
+        }
+
+        if (typeof (req.query.page) !== "string") {
+            return res.json({
+                success: true,
+                message: "page it's must a number."
+            })
+        }
+
+        const pageSize = parseInt(req.query.skip as string) || 5
+        const page: any = parseInt(req.query.page as string) || 1
+        const skip = (page - 1) * pageSize
+
+        const getAllMyAppointment = await Appointment.find({
+            where: { client_id: idToken },
+            relations: ["appointmentPortfolios", "artist"],
+            skip: skip,
+            take: pageSize
+        })
+
+        const appointmentsUser = await Promise.all(
+            getAllMyAppointment.map(async (obj) => {
+                const { status, artist_id, client_id, appointmentPortfolios, artist, ...rest } = obj;
+                const purchase = obj.appointmentPortfolios.map((obj) => obj.name)
+                const categoryPortfolio = obj.appointmentPortfolios.map((obj) => obj.category)
+                const getArtist = obj.artist
+
+                if (getArtist && (categoryPortfolio.length !== 0) && (purchase.length !== 0)) {
+                    const full_name = getArtist.full_name
+                    const email = getArtist.email;
+                    const is_active = getArtist.is_active;
+                    const name = purchase[0]
+                    const category = categoryPortfolio[0]
+                    return { full_name, email, name, category, is_active, ...rest };
+                }
+                else {
+                    return null
+                }
+            })
+        );
+
+        return res.json({
+            success: true,
+            message: "Here is a list of all your appointments.",
+            data: appointmentsUser
+        });
+    } catch (error) {
+        return res.json({
+            success: false,
+            message: "There was an issue retrieving your appointments, please try again",
+            error
+        });
+    }
+}
+
+
+const updateAppointment = async (req: Request, res: Response) => {
+    try {
+        const id = req.token.id;
+        const { date, shift, email, name } = req.body;
+        const dateRegex = /^\d{4}-\d{2}-\d{2}$/;
+        const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+        const today = new Date();
+
+        const year = today.getFullYear();
+        const month = today.getMonth() + 1;
+        const day = today.getDate() + 1;
+
+        // Formatear la fecha actual
+        const todayFormatDate = new Date(year, month - 1, day);
+
+        // Formatear la fecha de la cita
+        const appointmentDate = new Date(date);
+
+        console.log("1");
+
+        // Validar si la fecha de la cita es anterior a la actual
+        if (appointmentDate < todayFormatDate) {
+            console.log("2");
+            return res.json({
+                success: true,
+                message: "This day is prior to the current day, try again."
+            });
+        }
+
+        console.log("3");
+
+        // Verifica si la fecha proporcionada es válida según el formato
+        if (!date || typeof date !== "string" || !dateRegex.test(date)) {
+            return res.json({
+                success: true,
+                message: "Remember you must insert a date, and the date format should be YYYY-MM-DD, try again"
+            });
+        }
+        console.log("4")
+        // Verifica si se ha proporcionado un turno válido ('morning' o 'afternoon').
+        if (!shift || typeof shift !== "string" || (shift !== "morning" && shift !== "afternoon")) {
+            return res.json({
+                success: true,
+                message:"Remember you must insert a shift, and you only can put morning or afternoon, try again"
+            });
+        }
+        console.log("5")
+
+        // Verifica si el correo electrónico proporcionado es válido según un patrón predefinido
+        if (typeof email !== "string" || email.length > 100 || !emailRegex.test(email)) {
+            return res.json({
+                success: true,
+                message: 'Invalid or too long email'
+            });
+        }
+        console.log("6")
+
+        //Buscar al artista para ver si existe, si está activo, si tiene el role que corresponde y si no está cogiendo hora con él mismo. 
+        const foundArtistByEmail = await User.findOne({
+            where: { email },
+            relations: ["role"]
+        });
+        console.log("7")
+
+        if (!foundArtistByEmail || !foundArtistByEmail.is_active || foundArtistByEmail.role.role_name !== "admin" || id === foundArtistByEmail.id) {
+            return res.json({
+                success: true,
+                message: "Sorry, this user isn't an artist, or does not exist. And remember you can't create an appointment with yourself"
+            });
+        }
+
+        //Buscamos el nombre del servio tattoo/piercing para ver si coincide. Porque si no coincide no existe. 
+        const getService = await Portfolio.findOneBy({ name });
+
+        if (!getService) {
+            return res.json({
+                success: true,
+                message: "The name of the item purchase doesn't exist",
+            });
+        }
+
+        //Verificar que no exista una cita en la tabla Appointment con esa fecha y ese turno para ese artista.
+        const existingAppointment = await Appointment.findOne({
+            where: {
+                date,
+                shift,
+                artist_id: foundArtistByEmail.id,
+            },
+        });
+
+        if (existingAppointment) {
+            return res.json({
+                success: true,
+                message: "This appointment is not available, try again",
+            });
+        }
+
+        await Appointment.update({
+            id: id
+        }, {
+            date,
+            shift,
+            artist_id: foundArtistByEmail.id,
+        })
+
+        await Appointment_portfolio.update({
+            appointment_id: id
+        }, {
+            portfolio_id: getService?.id
+        })
+
+        const appointmentUpdated = await Appointment.findOneBy({
+            id: id
+        })
+
+        return res.json({
+            success: true,
+            message: "Appointment created succesfully",
+            data: {
+                date,
+                shift,
+                Worker: foundArtistByEmail.full_name,
+                email,
+                id: id,
+                name,
+                category: getService?.category,
+                created_at: appointmentUpdated?.created_at,
+                updated_at: appointmentUpdated?.updated_at
+            }
+        })
+
+    } catch (error) {
+        return res.json({
+            success: false,
+            message: "Appointment can't be created, try again",
+            error
+        });
+    }
+};
+
+export { createAppointment, myCalendarAsArtist, deleteAppointment, getAllMyAppointments, updateAppointment, getAllAppointmentsCalendar, getAllAppointmentsCalendarDetails }
--- src/routes/appointmentsRoutes.ts
+++ src/routes/appointmentsRoutes.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { auth } from "../middleware/auth";
-import { createAppointment, deleteAppointment, myCalendarAsArtist, getAllMyAppointments, getallAppointmentsAllUsers, updateAppointment } from "../controllers/appointmentControllers";
+import { createAppointment, deleteAppointment, myCalendarAsArtist, getAllMyAppointments, getAllAppointmentsCalendar, updateAppointment, getAllAppointmentsCalendarDetails } from "../controllers/appointmentControllers";
 import { isAdmin } from "../middleware/isAdmin";
 import { isSuperAdmin } from "../middleware/isSuperAdmin";
 
@@ -8,11 +8,12 @@ import { isSuperAdmin } from "../middleware/isSuperAdmin";
 const appointmentsRouter = Router();
 
 appointmentsRouter.post('/create',auth, createAppointment)
-appointmentsRouter.put('/update',auth, updateAppointment)
-appointmentsRouter.get('/get-all-my-appointments',auth, getAllMyAppointments)
 appointmentsRouter.get('/my-calendar-as-artist',auth, isAdmin, myCalendarAsArtist) 
 appointmentsRouter.delete('/delete-my-appointment',auth, deleteAppointment)
-// appointmentsRouter.get('appointment/getallAppointmentsAllUsers', auth, isSuperAdmin, getallAppointmentsAllUsers)
+appointmentsRouter.get('/all-appointments-calendar', auth, isSuperAdmin, getAllAppointmentsCalendar)
+appointmentsRouter.get('/all-appointments-calendar-detail', auth, isSuperAdmin, getAllAppointmentsCalendarDetails)
+appointmentsRouter.get('/get-all-my-appointments',auth, getAllMyAppointments)
+appointmentsRouter.put('/update',auth, updateAppointment)
 
 
 export {appointmentsRouter}
